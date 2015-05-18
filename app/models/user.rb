79686a35a278c5fa3a8ecb508824c29fc719e2e3
Lorena Fernández Martínez <lorena.fernandez@me.com>
2015-05-18 17:19:51 +0200

    Starting validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/    
-    
+
+  validates :name, presence: true, on: :update
+  validates :contact_person, format: {with: /\A[a-zA-Z]+\z+\s+/}, on: :update
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get '/que_es_tuarte' => 'site#que_es_tuarte', as: :que_es_tuarte
   get '/sign_up' => 'site#sign_up', as: :sign_up
-  get '/sign_in' => 'site#sign_in', as: :sign_in
+  #get '/sign_in' => 'site#sign_in', as: :sign_in
 
   get '/users/password/new' => 'devise/passwords#new', as: :password_update
   get '/users/sign_up' => 'devise/registrations#new', as: :sign_up_from
