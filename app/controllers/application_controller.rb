00d09b5ee701a17b01df95a3ddeb7cd2f8a70fb7
Lorena Fernández Martínez <lorena.fernandez@me.com>
2015-05-18 13:32:53 +0200

    RRSS added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
   	def configure_permitted_parameters
   		devise_parameter_sanitizer.for(:sign_up) << :name
-  		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :biography,:contact_person, :phone_number, :address, :province ,:town,:postal_code, :web  )}
+  		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :biography,:contact_person, :phone_number, :address, :province ,:town,:postal_code, :web, :twitter, :facebook, :instagram, :pinterest, :google_plus  )}
   	end
 
 
