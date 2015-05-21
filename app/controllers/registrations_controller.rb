1c6613064bce21feb109545f5d26af082c636b30
Lorena Fernández Martínez <lorena.fernandez@me.com>
2015-05-21 20:28:01 +0200

    Adding css

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
 
 	 def update
 	 	@user = current_user
-
     if @user.update_attributes(user_params)
       path = if @user.role == 'Artist'
         artist = @user.artist
