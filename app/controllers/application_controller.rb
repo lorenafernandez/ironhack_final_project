7b19098eebf409c9bcc6acc3b2c502081f8d56aa
Lorena Fernández Martínez <lorena.fernandez@me.com>
2015-05-22 22:52:13 +0200

    Solving bug in 'Edita tu perfil'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   def after_sign_in_path_for(resource)
      if current_user.role =='Artist' 
-        artist = current_user.create_artist
+        artist = Artist.find_by :user_id => params[:user_id]
         path = home_artist_path(artist)    
       else
-        local = current_user.create_local
+        local = Local.find_by :user_id => params[:user_id]
         path = home_local_path(local)
       end
       path
